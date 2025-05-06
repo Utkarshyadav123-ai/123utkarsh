a5ff0575b3adcf36306974b3fcb4037687021181
Utkarshyadav123-ai <utkarsh916968@gmail.com>
2025-05-06 22:51:06 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,10 @@
+(function() {
+
+    let screen = document.querySelector(`screen`);
+    let buttons = document.querySelector(`btn`);
+    let equal = document.querySelector(`btn-equal`);
+    let clear = document.querySelector(`btn-clear`);
+
+    buttons.forEach()
+}
+)
